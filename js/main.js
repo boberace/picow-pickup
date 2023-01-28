6ee31dea1587de957d53a6224757227a5badcf08
boberace <boberace@hotmail.com>
2023-01-28 08:47:57 -0500

    reorganized

---
+++ js/main.js
@@ -0,0 +1,40 @@
+window.onload = () => {
+    'use strict';
+  
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+               .register('./sw.js');
+    }
+  }
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('./service-worker.js');
+    });
+}
+
+var nbContainer = document.getElementById("icon-bar-left");
+var nbis = nbContainer.getElementsByClassName("nbi");
+for (var i = 0; i < nbis.length; i++) {
+    nbis[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+function popFun() {
+      var popup = document.getElementById("myPopup");
+      popup.classList.toggle("show");
+}
+
+var elem = document.getElementById("fs");
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,42 @@
+var cacheName = 'pp-pwa';
+var filesToCache = [
+    '/',
+    '/css',
+    '/html',
+    '/img',
+    '/index.html',
+    '/js',
+    '/manifest.json',
+    '/service-worker.js',
+    '/css/common.css',
+    '/css/popup.css',
+    '/css/style.css',
+    '/html/effect0.html',
+    '/html/effect1.html',
+    '/html/effect2.html',
+    '/html/effect3.html',
+    '/img/DAELAK-16x16.png',
+    '/img/DAELAK-192x192.png',
+    '/img/DAELAK-32x32.png',
+    '/img/DAELAK-512x512.png',
+    '/js/main.js' 
+];
+
+/* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('v1').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/css',
-                '/favicon.ico',
-                '/html',
-                '/img',
-                '/index.html',
-                '/js',
-                '/manifest.json',
-                '/css/common.css',
-                '/css/popup.css',
-                '/css/style.css',
-                '/html/effect0.html',
-                '/html/effect1.html',
-                '/html/effect2.html',
-                '/html/effect3.html',
-                '/img/DAELAK-16x16.png',
-                '/img/DAELAK-192x192.png',
-                '/img/DAELAK-32x32.png',
-                '/img/DAELAK-512x512.png',
-                '/service-worker.js'
-            ]);
-        })
+      caches.open(cacheName).then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
     );
-});
-
-self.addEventListener('fetch', function(event) {
+    self.skipWaiting();
+  });
+  
+  /* Serve cached content when offline */
+  self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
     );
-});
+  });
